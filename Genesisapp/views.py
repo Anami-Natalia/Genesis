93678d93edad65292e162efe340b28596c8546a2
Anami-Natalia <anaminatalia45@gmail.com>
2026-07-01 11:20:13 +0100

    update all

--- Genesisapp/views.py
+++ Genesisapp/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from Genesisapp.models import *
 
 # Create your views here.
 def details(request):
-    return render(request, 'blog-details.html')
+    return render(request, 'details.html')
 
 def home(request):
-    return render(request, 'index.html')
+    if request.method =='POST':
+        mycontact = Contact(
+           name = request.POST['name'],
+            email = request.POST['email'],
+            subject = request.POST['subject'],
+            message = request.POST['message']
+        )
+        mycontact.save()
+        return render(request, 'index.html')
+       
+    else:
+        return render(request, 'index.html')
 
 def blog(request):
     return render(request, 'blog.html')
